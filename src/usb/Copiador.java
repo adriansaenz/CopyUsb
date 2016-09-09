02c3a5acaee58df45a06835c404d7271755ba779
adriansaenz <adriansaenz15995@gmail.com>
2016-09-09 21:06:28 +0200

    Añadido comentarios

--- src/usb/Copiador.java
+++ src/usb/Copiador.java
@@ -1,19 +1,25 @@
+/*
+Esta clase contiene los metodos para buscar los archivos del pendrive 
+y copiarlos en el escritorio.
+------------------------------------------------------
+This class includes the methods for search the pendrive files
+and copy it in the desktop.
+*/
 package usb;
 
-import java.awt.Component;
 import java.io.*;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
 public class Copiador {
-    private String archO, archD, dirO, dirD;
+    
     private File dir;
     
     public Copiador(File dir){
         this.dir = dir;
         buscar(dir);
     }
-    
+    /*
+    Esta funcion copia un archivo desde la direccion origen a la direccion destino.
+    */
     private void copiar(String origen, String destino){
         try{
         File archivo = new File(destino);
@@ -32,14 +38,19 @@ public class Copiador {
         }catch(IOException e){ System.out.println("error: "+e.getMessage()); }
     }
     
+    /*
+    Esta funcion lista todo los ficheros que contiene el pendrive. 
+    Si es archivo lo copia en la carpeta del escritorio. 
+    Si es directorio vuelve a llamar a buscar con el nuevo directorio.
+    */
     private void buscar(File dir){
         File f;
         File[] lista = dir.listFiles();
         for (File elemento : lista){
             String name = elemento.getAbsolutePath().substring(3);
-            if(elemento.isFile()) copiar(elemento.getAbsolutePath(),"C:\\Users\\Adrian\\Desktop\\asd\\"+name);
+            if(elemento.isFile()) copiar(elemento.getAbsolutePath(),"C:\\Users\\Adrian\\Desktop\\Archivos\\"+name);
             else if(elemento.isDirectory()){
-                f = new File("C:\\Users\\Adrian\\Desktop\\asd\\"+name+"\\");
+                f = new File("C:\\Users\\Adrian\\Desktop\\Archivos\\"+name+"\\");
                 f.mkdir();
                 buscar(elemento);
             }
